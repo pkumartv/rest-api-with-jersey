3e93f99fa2f8b9775c11e6a90d8569d1732fb60d
pavan tiruvuri <pavan.tiruvuri@oracle.com>
2021-03-11 11:25:08 +0530

    Updated message service with a new path /messageId

--- messenger/src/main/java/org/rest/messenger/resource/MessageResource.java
+++ messenger/src/main/java/org/rest/messenger/resource/MessageResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -13,13 +14,20 @@ import org.rest.messenger.service.MessageService;
 @Path("messages")
 public class MessageResource{
 
-    MessageService ms= new MessageService();
-
+    MessageService messageService= new MessageService();
 
     @GET
     @Produces(MediaType.APPLICATION_XML)
     public List<Message> getMessages(){
-        return ms.getMessages();
+        return messageService.getAllMessages();
+    }
+
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Message test(@PathParam("messageId") long id){
+        Message message= messageService.getMessage(id);
+        return message;
     }
 
 }
--- messenger/src/main/java/org/rest/messenger/service/MessageService.java
+++ messenger/src/main/java/org/rest/messenger/service/MessageService.java
@@ -2,19 +2,43 @@ package org.rest.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.rest.messenger.database.DatabaseClass;
 import org.rest.messenger.model.Message;
 
 public class MessageService {
-    
-    public List<Message> getMessages(){
-        List<Message> messageList= new ArrayList<Message>();
-        Message m1= new Message(1L, "Hola Amigos", "Pavan");
-        Message m2= new Message(2L, "Hello folks", "Kiran");
 
-        messageList.add(m1);
-        messageList.add(m2);
+    private Map<Long, Message> messages = DatabaseClass.getMessages();
 
-        return messageList;
+    public MessageService(){
+        messages.put(1L,new Message(1, "Hola Amigos", "Pavan"));
+        messages.put(2L,new Message(2, "Hello folks", "Kiran"));
+    }
+
+    public List<Message> getAllMessages() {
+        return new ArrayList<Message>(messages.values());
+    }
+
+    public Message getMessage(long id) {
+        return messages.get(id);
+    }
+
+    public Message addMessage(Message message) {
+        message.setId(messages.size() + 1);
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message updateMessage(Message message) {
+        if (message.getId() <= 0)
+            return null;
+
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message removeMessage(long id) {
+        return messages.remove(id);
     }
 }
