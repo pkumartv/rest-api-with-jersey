1e381030156a12be2d8739fe5bff0188cc611160
pavan tiruvuri <pavan.tiruvuri@oracle.com>
2021-03-11 14:12:47 +0530

    Added support for update and delete message

--- messenger/src/main/java/org/rest/messenger/resource/MessageResource.java
+++ messenger/src/main/java/org/rest/messenger/resource/MessageResource.java
@@ -2,7 +2,11 @@ package org.rest.messenger.resource;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,17 +21,41 @@ public class MessageResource{
     MessageService messageService= new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages(){
         return messageService.getAllMessages();
     }
 
     @GET
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_JSON)
     public Message test(@PathParam("messageId") long id){
         Message message= messageService.getMessage(id);
         return message;
     }
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message addMessage(Message message){
+        return messageService.addMessage(message);
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{messageId}")
+    public Message updateMessage(@PathParam("messageId") long id,Message message){
+        message.setId(id);
+        return messageService.updateMessage(message);
+    }
+
+
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{messageId}")
+    public Message deleteMessage(@PathParam("messageId") long id){
+        return messageService.removeMessage(id);
+    }
 }
