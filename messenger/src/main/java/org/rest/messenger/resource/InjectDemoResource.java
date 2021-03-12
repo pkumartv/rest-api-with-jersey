1b30dca2cda8a55b3e00abd6b1913b71110ff617
pavan tiruvuri <pavan.tiruvuri@oracle.com>
2021-03-12 09:33:28 +0530

    Using Context annotation

--- messenger/src/main/java/org/rest/messenger/resource/InjectDemoResource.java
+++ messenger/src/main/java/org/rest/messenger/resource/InjectDemoResource.java
@@ -7,7 +7,10 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("injectdemo")
 @Consumes(MediaType.TEXT_PLAIN)
@@ -23,6 +26,16 @@ public class InjectDemoResource {
     {
         return "MatrixParam: "+matricParam + "\n customHeader: "+ customHeaderValue
         + "\n cookie: " + cookie;
+    }
+
+    @GET
+    @Path("context")
+    public String getParamsUsingContext(@Context UriInfo uriInfo,
+                                        @Context HttpHeaders headers){
+        String path = uriInfo.getAbsolutePath().toString();
+        String cookies=headers.getCookies().toString();
+        return "Path: "+ path + " Cookies: "+ cookies;
+
     }
 
     
