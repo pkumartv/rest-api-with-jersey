55a2ded5f5b2ac11e0db4d5af2203609542ee2db
pavan tiruvuri <pavan.tiruvuri@oracle.com>
2021-03-12 23:23:52 +0530

    Support for Status codes and location  headers

--- messenger/src/main/java/org/rest/messenger/resource/MessageResource.java
+++ messenger/src/main/java/org/rest/messenger/resource/MessageResource.java
@@ -1,5 +1,6 @@
 package org.rest.messenger.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.ws.rs.BeanParam;
@@ -11,7 +12,11 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
 
 import org.rest.messenger.model.Message;
 import org.rest.messenger.resource.beans.MessageFilterBean;
@@ -44,8 +49,18 @@ public class MessageResource {
     }
 
     @POST
-    public Message addMessage(Message message) {
-        return messageService.addMessage(message);
+    public Response addMessage(Message message,@Context UriInfo uriInfo) {
+        // return Response.status(Status.CREATED)
+        //         .entity(newMessage)
+        //         .build();
+
+        Message newMessage= messageService.addMessage(message);
+        String newId=String.valueOf(newMessage.getId());
+        URI uri= uriInfo.getAbsolutePathBuilder().path(newId).build();
+        return Response.created(uri)
+                        .entity(newMessage)
+                        .build();                
+        //return messageService.addMessage(message);
     }
 
     @PUT
