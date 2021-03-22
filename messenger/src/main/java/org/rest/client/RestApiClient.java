16395dcac99927438945ba47fccfb7917e84abbe
pavan tiruvuri <pavan.tiruvuri@oracle.com>
2021-03-22 22:08:30 +0530

    Best practices

--- messenger/src/main/java/org/rest/client/RestApiClient.java
+++ messenger/src/main/java/org/rest/client/RestApiClient.java
@@ -13,17 +13,30 @@ public class RestApiClient {
 
     public static void main(String[] args) {
         Client client = ClientBuilder.newClient();
-        Message message = client
-                    .target("http://localhost:8080/messenger/webapi/messages/2")
-                    .request(MediaType.APPLICATION_JSON)
-                    .get(Message.class);
+
         // Split
         // WebTarget target =
         // client.target("http://localhost:8080/messenger/webapi/messages/1");
         // Builder builder= target.request();
         // Response response=builder.get();
         //Message message = response.readEntity(Message.class);
-        System.out.println("response " + message.getMessage());
+
+
+        WebTarget baseTarget = client.target("http://localhost:8080/messenger/webapi/");
+        WebTarget messagesTarget = baseTarget.path("messages");
+        WebTarget singleMessagesTarget = messagesTarget.path("{messageId}");
+
+        Message message1 = singleMessagesTarget.resolveTemplate("messageId", 1)
+                    .request(MediaType.APPLICATION_JSON)
+                    .get(Message.class);
+
+        System.out.println("response " + message1.getMessage());
+
+        Message message2 = singleMessagesTarget.resolveTemplate("messageId", 2)
+        .request(MediaType.APPLICATION_JSON)
+        .get(Message.class);
+
+        System.out.println("response " + message2.getMessage());
     }
 
 }
